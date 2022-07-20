78aca72e971f724fce3d12944b8a119ee68c737b
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 12:17:20 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmAngryBirdsVB
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pcbCerdo1 = New System.Windows.Forms.PictureBox()
+        Me.pcbPajaro = New System.Windows.Forms.PictureBox()
+        Me.tmrTiempo = New System.Windows.Forms.Timer(Me.components)
+        Me.lblVidas = New System.Windows.Forms.Label()
+        Me.lblScoreTxt = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.pcbCerdo2 = New System.Windows.Forms.PictureBox()
+        Me.pcbCerdo3 = New System.Windows.Forms.PictureBox()
+        CType(Me.pcbCerdo1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pcbPajaro, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pcbCerdo2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pcbCerdo3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pcbCerdo1
+        '
+        Me.pcbCerdo1.BackColor = System.Drawing.Color.Transparent
+        Me.pcbCerdo1.BackgroundImage = Global.AngryBirdsVB.My.Resources.Resources._4
+        Me.pcbCerdo1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pcbCerdo1.Location = New System.Drawing.Point(645, 395)
+        Me.pcbCerdo1.Name = "pcbCerdo1"
+        Me.pcbCerdo1.Size = New System.Drawing.Size(100, 100)
+        Me.pcbCerdo1.TabIndex = 1
+        Me.pcbCerdo1.TabStop = False
+        '
+        'pcbPajaro
+        '
+        Me.pcbPajaro.BackColor = System.Drawing.Color.Transparent
+        Me.pcbPajaro.BackgroundImage = Global.AngryBirdsVB.My.Resources.Resources._3
+        Me.pcbPajaro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pcbPajaro.Location = New System.Drawing.Point(139, 375)
+        Me.pcbPajaro.Name = "pcbPajaro"
+        Me.pcbPajaro.Size = New System.Drawing.Size(100, 100)
+        Me.pcbPajaro.TabIndex = 0
+        Me.pcbPajaro.TabStop = False
+        '
+        'tmrTiempo
+        '
+        Me.tmrTiempo.Interval = 1
+        '
+        'lblVidas
+        '
+        Me.lblVidas.AutoSize = True
+        Me.lblVidas.Font = New System.Drawing.Font("Bauhaus 93", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblVidas.Location = New System.Drawing.Point(31, 24)
+        Me.lblVidas.Name = "lblVidas"
+        Me.lblVidas.Size = New System.Drawing.Size(109, 30)
+        Me.lblVidas.TabIndex = 2
+        Me.lblVidas.Text = "Vidas: 5"
+        '
+        'lblScoreTxt
+        '
+        Me.lblScoreTxt.AutoSize = True
+        Me.lblScoreTxt.Font = New System.Drawing.Font("Bauhaus 93", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScoreTxt.Location = New System.Drawing.Point(33, 67)
+        Me.lblScoreTxt.Name = "lblScoreTxt"
+        Me.lblScoreTxt.Size = New System.Drawing.Size(95, 30)
+        Me.lblScoreTxt.TabIndex = 3
+        Me.lblScoreTxt.Text = "Score: "
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Bauhaus 93", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(122, 67)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(28, 30)
+        Me.lblScore.TabIndex = 4
+        Me.lblScore.Text = "0"
+        '
+        'pcbCerdo2
+        '
+        Me.pcbCerdo2.BackColor = System.Drawing.Color.Transparent
+        Me.pcbCerdo2.BackgroundImage = Global.AngryBirdsVB.My.Resources.Resources._4
+        Me.pcbCerdo2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pcbCerdo2.Location = New System.Drawing.Point(783, 395)
+        Me.pcbCerdo2.Name = "pcbCerdo2"
+        Me.pcbCerdo2.Size = New System.Drawing.Size(100, 100)
+        Me.pcbCerdo2.TabIndex = 5
+        Me.pcbCerdo2.TabStop = False
+        '
+        'pcbCerdo3
+        '
+        Me.pcbCerdo3.BackColor = System.Drawing.Color.Transparent
+        Me.pcbCerdo3.BackgroundImage = Global.AngryBirdsVB.My.Resources.Resources._4
+        Me.pcbCerdo3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pcbCerdo3.Location = New System.Drawing.Point(922, 395)
+        Me.pcbCerdo3.Name = "pcbCerdo3"
+        Me.pcbCerdo3.Size = New System.Drawing.Size(100, 100)
+        Me.pcbCerdo3.TabIndex = 6
+        Me.pcbCerdo3.TabStop = False
+        '
+        'frmAngryBirdsVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.AngryBirdsVB.My.Resources.Resources.Sin_título
+        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.Controls.Add(Me.pcbCerdo3)
+        Me.Controls.Add(Me.pcbCerdo2)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.lblScoreTxt)
+        Me.Controls.Add(Me.lblVidas)
+        Me.Controls.Add(Me.pcbCerdo1)
+        Me.Controls.Add(Me.pcbPajaro)
+        Me.Name = "frmAngryBirdsVB"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Angry Birds"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.pcbCerdo1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pcbPajaro, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pcbCerdo2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pcbCerdo3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pcbCerdo1 As PictureBox
+    Friend WithEvents pcbPajaro As PictureBox
+    Friend WithEvents tmrTiempo As Timer
+    Friend WithEvents lblVidas As Label
+    Friend WithEvents lblScoreTxt As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents pcbCerdo2 As PictureBox
+    Friend WithEvents pcbCerdo3 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports System.IO.Ports
+Imports System.IO
+
+Public Class frmAngryBirdsVB
+
+
+
+    Dim agarre, cerdo1, cerdo2, cerdo3 As Boolean
+    Dim posicionX, posicionY, xi, yi, xf, yf, fuerza, score, resorteraX, resorteraY, cerdoX1, cerdoY1, cerdoX2, cerdoY2, cerdoX3, cerdoY3, pruebaX, pruebaY As Integer
+
+    Private Sub frmAngryBirdsVB_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dim guardar As String = "C:\Users\CarlosLap\Documents\Visual Studio 2019\Projects\19- AngryBirds\guardar\variables.txt"
+        Dim escritor As New StreamWriter(guardar)
+        escritor.WriteLine(pcbCerdo1.Left & "," & pcbCerdo2.Left & "," & pcbCerdo3.Left & "," & pcbCerdo1.Top & "," & pcbCerdo2.Top & "," & pcbCerdo3.Top & "," & cerdo1 & "," & cerdo2 & "," & cerdo3 & "," & score & "," & tmrTiempo.Enabled & "," & pcbPajaro.Left & "," & pcbPajaro.Top & "," & vidas & "," & angulo & "," & tiempo & "," & pruebaX & "," & pruebaY & "," & fuerza)
+        escritor.Close()
+    End Sub
+
+    Private Sub frmAngryBirdsVB_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+
+    End Sub
+
+    Dim angulo, tiempo As Double
+    Dim vidas As Integer = 5
+
+    'Cargar form
+    Private Sub frmAngryBirdsVB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.bird, AudioPlayMode.BackgroundLoop)
+
+        Dim abrir As String = "C:\Users\CarlosLap\Documents\Visual Studio 2019\Projects\19- AngryBirds\guardar\variables.txt"
+
+        If System.IO.File.Exists(abrir) = True Then
+            Dim lector As New StreamReader(abrir)
+
+            pcbCerdo1.Left = lector.ReadLine()
+            lector.Close()
+
+        Else
+            MsgBox("No existe el archivo")
+            cerdos()
+        End If
+
+
+    End Sub
+
+
+
+
+    'Agarre
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles pcbPajaro.MouseDown
+
+        If tmrTiempo.Enabled = False Then
+            agarre = True
+            posicionX = MousePosition.X - sender.left
+            posicionY = MousePosition.Y - sender.top
+
+            xi = MousePosition.X
+            yi = MousePosition.Y
+        End If
+
+
+    End Sub
+
+
+    'Arrastre
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles pcbPajaro.MouseMove
+
+        If agarre = True And tmrTiempo.Enabled = False And MousePosition.X < 500 And MousePosition.Y > 300 Then
+            pcbPajaro.Left = MousePosition.X - posicionX
+            pcbPajaro.Top = MousePosition.Y - posicionY
+        End If
+
+
+    End Sub
+
+
+
+    'Soltar
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles pcbPajaro.MouseUp
+
+        If tmrTiempo.Enabled = False Then
+
+            agarre = False
+            tiempo = 0
+            tmrTiempo.Enabled = True
+
+            If MousePosition.X < 500 Then
+                xf = MousePosition.X
+                pruebaX = MousePosition.X - posicionX
+            Else
+                xf = pcbPajaro.Left
+                pruebaX = pcbPajaro.Left - posicionX
+            End If
+
+
+
+            If MousePosition.Y > 300 Then
+                yf = MousePosition.Y
+                pruebaY = MousePosition.Y - posicionY
+            Else
+                yf = pcbPajaro.Top
+                pruebaY = pcbPajaro.Top - posicionY
+            End If
+
+
+
+            'Fuerza
+            fuerza = Math.Sqrt((xf - xi) ^ 2 + (yf - yi) ^ 2)
+
+
+            'Angulo
+            angulo = Math.Asin((yf - yi) / (fuerza)) * (180 / 3.141592)
+
+        End If
+
+
+
+    End Sub
+
+
+
+    Private Sub tmrTiempo_Tick(sender As Object, e As EventArgs) Handles tmrTiempo.Tick
+
+        tiempo += 0.1
+
+        Dim X, Y As Integer
+
+        If pruebaX < 139 Then
+            X = (fuerza * (Math.Cos(angulo * 3.141592 / 180)) * tiempo) + pruebaX
+        Else
+            X = (-1) * (fuerza * (Math.Cos(angulo * 3.141592 / 180)) * tiempo) + pruebaX
+        End If
+
+        If pruebaY < 375 Then
+            Y = pruebaY + ((fuerza * (Math.Sin(angulo * 3.141592 / 180)) * tiempo - ((9.81 * (tiempo ^ 2)) / 2)) * -1)
+        Else
+            Y = pruebaY - (fuerza * (Math.Sin(angulo * 3.141592 / 180)) * tiempo - ((9.81 * (tiempo ^ 2)) / 2))
+        End If
+
+
+        Me.Text = "X: " & X & "     Y: " & Y & "       Angulo: " & angulo
+
+
+        pcbPajaro.Left = X
+        pcbPajaro.Top = Y
+
+
+
+
+        'Te sales de la pantalla
+        If X > 1366 Or X < -250 Or Y > 768 Then
+            vidas -= 1
+            lblVidas.Text = "Vidas: " & vidas
+            pcbPajaro.Left = 139
+            pcbPajaro.Top = 375
+            tmrTiempo.Enabled = False
+        End If
+
+
+
+
+
+
+
+        'Si golpeas a los CERDOS
+        If pcbPajaro.Bounds.IntersectsWith(pcbCerdo1.Bounds) Then
+            pcbCerdo1.Location = New Point(4000, 4000)
+            score += 100
+            lblScore.Text = score
+            cerdo1 = True
+        End If
+
+        If pcbPajaro.Bounds.IntersectsWith(pcbCerdo2.Bounds) Then
+            pcbCerdo2.Location = New Point(4000, 4000)
+            score += 100
+            lblScore.Text = score
+            cerdo2 = True
+        End If
+
+        If pcbPajaro.Bounds.IntersectsWith(pcbCerdo3.Bounds) Then
+            pcbCerdo3.Location = New Point(4000, 4000)
+            score += 100
+            lblScore.Text = score
+            cerdo3 = True
+        End If
+
+        If cerdo1 = True And cerdo2 = True And cerdo3 = True Then
+            tmrTiempo.Enabled = False
+            If MsgBox("Ganaste!, jugar de nuevo?", MsgBoxStyle.YesNo) = 6 Then
+                tmrTiempo.Enabled = False
+                lblScore.Text = score
+                vidas = 5
+                lblVidas.Text = "Vidas: " & vidas
+                pcbPajaro.Left = 139
+                pcbPajaro.Top = 375
+
+                cerdos()
+            Else
+                Application.Exit()
+                Me.Close()
+            End If
+
+        End If
+
+        If vidas <= 0 Then
+            tmrTiempo.Enabled = False
+            If MsgBox("Perdiste!, jugar de nuevo?", MsgBoxStyle.YesNo) = 6 Then
+                lblScore.Text = score
+                vidas = 5
+                lblVidas.Text = "Vidas: " & vidas
+                pcbPajaro.Left = 139
+                pcbPajaro.Top = 375
+                tmrTiempo.Enabled = False
+                cerdos()
+            Else
+                Application.Exit()
+                Me.Close()
+
+            End If
+
+        End If
+
+    End Sub
+
+
+
+    Sub cerdos()
+
+
+        'CERDO1
+        Randomize()
+        cerdoX1 = Convert.ToInt32((Rnd() * 1200))
+        If cerdoX1 < 540 Then
+            cerdoX1 += 540
+        End If
+        cerdo1 = False
+
+        Randomize()
+
+        cerdoY1 = Convert.ToInt32(Rnd() * 560)
+        If cerdoY1 < 10 Then
+            cerdoY1 += 10
+        End If
+
+        pcbCerdo1.Left = cerdoX1
+        pcbCerdo1.Top = cerdoY1
+
+
+
+
+        'CERDO2
+        Randomize()
+        cerdoX2 = Convert.ToInt32((Rnd() * 1200))
+        If cerdoX2 < 540 Then
+            cerdoX2 += 540
+        End If
+        cerdo2 = False
+
+        Randomize()
+
+        cerdoY2 = Convert.ToInt32(Rnd() * 560)
+        If cerdoY2 < 10 Then
+            cerdoY2 += 10
+        End If
+
+        pcbCerdo2.Left = cerdoX2
+        pcbCerdo2.Top = cerdoY2
+
+
+
+
+        'CERDO3
+        Randomize()
+        cerdoX3 = Convert.ToInt32((Rnd() * 1200))
+        If cerdoX3 < 540 Then
+            cerdoX3 += 540
+        End If
+        cerdo3 = False
+
+        Randomize()
+
+        cerdoY3 = Convert.ToInt32(Rnd() * 560)
+        If cerdoY3 < 10 Then
+            cerdoY3 += 10
+        End If
+
+        pcbCerdo3.Left = cerdoX3
+        pcbCerdo3.Top = cerdoY3
+
+    End Sub
+
+End Class
